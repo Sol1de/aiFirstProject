9b0d46e136c4c7d07a6a179ec3b7e3adc1f0ac03
Sol1de <olivier.delmas@edu.devinci.fr>
2025-03-19 14:20:20 +0100

    :fire: refacto server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const path = require('path');
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -7,5 +11,5 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-    console.log(`Serveur lancé sur http://localhost:${port}`);
+    console.log(`server launched on : http://localhost:${port}`);
 });
